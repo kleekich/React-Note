293f231c492bd62f8e5f68cb15bc82c0111a6d4e
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2017-08-17 14:55:12 -0700

    data of user input from form/input tag correctly transfer with ref attribute

--- src/components/create-todos.js
+++ src/components/create-todos.js
@@ -7,10 +7,15 @@ import React from 'react';
 export default class TodosList extends React.Component {
 	render() {
 		return (
-			<form>
-				<input type="text" placeholder="What do I need to do?" />
+			<form onSubmit={this.handleCreate.bind(this)}>
+				<input type="text" placeholder="What do I need to do?" ref="createInput"/>
 				<button>Create</button>
 			</form>
 		);
+	}
+
+	handleCreate(event){
+		event.preventDefault();
+		console.log(this.refs.createInput.value);
 	}
 }
