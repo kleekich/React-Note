79b9a683c75c775c1c0fe36fa17adecb90c5fbc5
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2017-08-17 14:50:16 -0700

    onCancelClick state correctly works

--- src/components/todos-list-item.js
+++ src/components/todos-list-item.js
@@ -14,7 +14,7 @@ export default class TodosListItem extends React.Component {
 			return(
 				<td>
 					<button>Save</button>
-					<button>Cancel</button>
+					<button onClick={this.onCancelClick.bind(this)}>Cancel</button>
 				</td>
 			);
 		}
@@ -40,5 +40,8 @@ export default class TodosListItem extends React.Component {
 
 	onEditClick() {
 		this.setState({isEditing: true});
+	}
+	onCancelClick(){
+		this.setState({isEditing: false});
 	}
 }
